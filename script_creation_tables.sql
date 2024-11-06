773236af4f139dab25355ae5e1fdd0ba2ce778ee
ClemDW <clementdewasch54@gmail.com>
2024-11-06 16:38:47 +0100

    script sql

--- script_creation_tables.sql
+++ script_creation_tables.sql
@@ -1,15 +1,16 @@
+CREATE TABLE Soiree (
+    idSoiree INT(4) PRIMARY KEY,
+    idLieu INT(4),
+    estAnnule BOOLEAN DEFAULT FALSE,
+    dateSoiree DATE,
+    FOREIGN KEY (idLieu) REFERENCES Lieu(idLieu)
+);
+
 CREATE TABLE Lieu (
     idLieu INT(4) PRIMARY KEY,
     nomLieu VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Soiree (
-    idSoiree INT(4) PRIMARY KEY,
-    idLieu INT(4),
-    estAnnule BOOLEAN DEFAULT FALSE,
-    dateSoiree DATE NOT NULL,
-    FOREIGN KEY (idLieu) REFERENCES Lieu(idLieu) ON DELETE SET NULL
-);
 
 CREATE TABLE Spectacle (
     idSpectacle INT(4) PRIMARY KEY,
@@ -19,16 +20,16 @@ CREATE TABLE Spectacle (
 CREATE TABLE Programme (
     idSoiree INT(4),
     idSpectacle INT(4),
-    heureD DATE NOT NULL,
-    heureF DATE NOT NULL,
+    heureD DATE,
+    heureF DATE,
     PRIMARY KEY (idSoiree, idSpectacle),
-    FOREIGN KEY (idSoiree) REFERENCES Soiree(idSoiree) ON DELETE CASCADE,
-    FOREIGN KEY (idSpectacle) REFERENCES Spectacle(idSpectacle) ON DELETE CASCADE
+    FOREIGN KEY (idSoiree) REFERENCES Soiree(idSoiree),
+    FOREIGN KEY (idSpectacle) REFERENCES Spectacle(idSpectacle)
 );
 
 CREATE TABLE Utilisateur (
     idUtilisateur INT(4) PRIMARY KEY,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    mdp VARCHAR(256) NOT NULL,
-    role INT(3) NOT NULL
+    email VARCHAR(50),
+    mdp VARCHAR(256),
+    role INT(3)
 );
