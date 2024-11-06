a52f23cfdb6ad6d9d4bd197f631bf1d7ca2aa0d2
Kuuro <150788832+Kuuro57@users.noreply.github.com>
2024-11-06 16:40:59 +0100

    Modification script_creation_tables.sql

--- script_creation_tables.sql
+++ script_creation_tables.sql
@@ -1,27 +1,26 @@
-CREATE TABLE Soiree (
-    idSoiree INT(4) PRIMARY KEY,
-    idLieu INT(4),
-    estAnnule BOOLEAN DEFAULT FALSE,
-    dateSoiree DATE,
-    FOREIGN KEY (idLieu) REFERENCES Lieu(idLieu)
-);
-
 CREATE TABLE Lieu (
     idLieu INT(4) PRIMARY KEY,
     nomLieu VARCHAR(50) NOT NULL
 );
-
 
 CREATE TABLE Spectacle (
     idSpectacle INT(4) PRIMARY KEY,
     style VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE Soiree (
+    idSoiree INT(4) PRIMARY KEY,
+    idLieu INT(4),
+    estAnnule BOOLEAN NOT NULL DEFAULT FALSE,
+    dateSoiree DATE NOT NULL,
+    FOREIGN KEY (idLieu) REFERENCES Lieu(idLieu)
+);
+
 CREATE TABLE Programme (
     idSoiree INT(4),
     idSpectacle INT(4),
-    heureD DATE,
-    heureF DATE,
+    heureD DATE NOT NULL,
+    heureF DATE NOT NULL,
     PRIMARY KEY (idSoiree, idSpectacle),
     FOREIGN KEY (idSoiree) REFERENCES Soiree(idSoiree),
     FOREIGN KEY (idSpectacle) REFERENCES Spectacle(idSpectacle)
@@ -29,7 +28,7 @@ CREATE TABLE Programme (
 
 CREATE TABLE Utilisateur (
     idUtilisateur INT(4) PRIMARY KEY,
-    email VARCHAR(50),
-    mdp VARCHAR(256),
-    role INT(3)
+    email VARCHAR(50) UNIQUE NOT NULL,
+    mdp VARCHAR(256) NOT NULL,
+    role INT(3) NOT NULL
 );
