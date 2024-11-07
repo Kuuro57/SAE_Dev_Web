70b8e2ed347b574e5d2c977076e495215e13f193
armbrust5u <SuperBidul1@gmail.com>
2024-11-07 16:13:04 +0100

    sauvegarde

--- src/repository/SelectRepository.php
+++ src/repository/SelectRepository.php
@@ -37,7 +37,8 @@ class SelectRepository extends Repository {
      */
     //null TODO switch sur le filtre qui renvoie une requête SQL différente en fonction du filtre
     public function getSpectacles(string $filtre) : array {
-        
+        return [];
+        //TODO
     }
 
 
@@ -48,10 +49,66 @@ class SelectRepository extends Repository {
      * @return Spectacle Un objet de type spectacle
      */
     public function getSpectacle(int $id) : Spectacle {
-        // TODO
+        // Requête SQL qui récupère l'id du spectacle
+        $querySQL = "SELECT idSpectale FROM spectacle WHERE idSpectacle = ?";
+        // Préparation de la requête
+        $statement = $this->pdo->prepare($querySQL);
+        $statement->bindParam(1, $id);
+        // Execution de la requête
+        $statement->execute();
+
+        // On récupère les données sorties par la requête
+        $data = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $spectacle = $this->findSpectacle((int) $data['idSpectacle']);
+
+        return $spectacle;
     }
 
+    public function findPlaylist(int $id) : Playlist {
+        $querySQL1 = 'SELECT id, nom FROM playlist WHERE id = :id';
 
+        $statement1 = $this->pdo->prepare($querySQL1);
+        $statement1->execute(['id' => $id]);
+
+        $row = $statement1->fetch();
+        $pl = new Playlist($row['nom'], []);
+        $pl->setId((int)$row['id']); // associe l'id à la playlist récupérée
+
+
+
+        $querySQL2 = 'SELECT * FROM playlist2track INNER JOIN track ON playlist2track.id_track = track.id
+                      WHERE id_pl = :id';
+
+        $statement2 = $this->pdo->prepare($querySQL2);
+        $statement2->execute(['id' => $id]);
+
+
+        foreach ($statement2->fetchAll() as $row) {
+            if ($row['type'] === 'P') {
+                $podcastTrack = new PodcastTrack($row['titre'], $row['filename']);
+                $podcastTrack->setId((int)$row['id']);
+                $podcastTrack->setGenre($row['genre']);
+                $podcastTrack->setDuree((int)$row['duree']);
+                $podcastTrack->setAuteur($row['auteur_podcast']);
+                $podcastTrack->setDate($row['date_posdcast']);
+
+                $pl->addTrack($podcastTrack);
+            }
+            else if ($row['type'] === 'A') {
+                $albumTrack = new AlbumTrack($row['titre'], $row['filename'], $row['titre_album'], (int)$row['id']);
+                $albumTrack->setArtiste($row['artiste_album']);
+                $albumTrack->setAnnee((int)$row['annee_album']);
+                $albumTrack->setGenre($row['genre']);
+                $albumTrack->setDuree((int)$row['duree']);
+
+                $pl->addTrack($albumTrack);
+            }
+
+        }
+
+        return $pl;
+    }
 
     /**
      * Méthode qui regarde si l'email est déjà présent dans la BDD
@@ -60,8 +117,30 @@ class SelectRepository extends Repository {
      *               présente dans la BDD, une liste vide si l'utilisateur n'existe pas
      */
     public function findExistingEmail(string $email) : array {
-        // TODO
+        // Requête SQL
+        $querySQL = "SELECT mdp, role FROM utilisateur WHERE email = ? ";
+        // Préparation de la requête
+        $statement = $this->pdo->prepare($querySQL);
+        $statement->bindParam(1, $email);
+        // Execution de la requête
+        $statement->execute();
+        // On récupère les données sorties par la requête
+        $data = $statement->fetch(PDO::FETCH_ASSOC);
+
+        // Si les données sont vides
+        if (empty($data)) {
+            // Le résultat est une liste vide
+            $res = [];
+        }
+        //Sinon
+        else {
+            // Le résultat est une liste contenant le mot de passe hashé correspondant à l'email et son role
+            $res = [
+                'mdp' => $data['mdp'],
+                'role' => $data['role']
+            ];
+        }
+        // On retourne le résultat
+        return $res;
     }
-
-
 }
