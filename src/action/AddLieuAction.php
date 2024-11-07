64235dee6ab75ba2de516474aae8a7d138c513ce
OscarTom54 <oscartom54@gmail.com>
2024-11-07 16:03:29 +0100

    AddLieuAction execute et form OK

--- src/action/AddLieuAction.php
+++ src/action/AddLieuAction.php
@@ -2,8 +2,8 @@
 
 namespace iutnc\sae_dev_web\action;
 
-use iutnc\sae_dev_web\action\Action;
-
+use iutnc\sae_dev_web\repository\InsertRepository;
+use PDOException;
 
 
 /**
@@ -11,15 +11,31 @@ use iutnc\sae_dev_web\action\Action;
  */
 class AddLieuAction extends Action {
 
-    // Attribut
     private string $formulaire = '<form method="post" action="?action=add-lieu">
-                                        // TODO
-                                  </form>';
+                        <input type="text" name="FnomLieu" placeholder="Nom" class="input-field" required autofocus>
+                        <input type="text" name="Fadresse" placeholder="Adresse" class="input-field" required>
+                        <input type="number" name="FplacesAssises" placeholder="Capacité Assise" class="input-field" required>
+                        <input type="number" name="FplacesDebout" placeholder="Capacité Debout" class="input-field" required>
+                        <input type="submit" name="connex" value="Connexion" class="button">
+                    </form>';
 
+    public function execute(): string {
+        if ($this->http_method == "GET") {
+            $res = '<h1>Ajouter un lieu</h1>' . $this->formulaire;
+        } else {
+            $nomLieu = filter_var($_POST['FnomLieu'], FILTER_SANITIZE_STRING);
+            $adresse = filter_var($_POST['Fadresse'], FILTER_SANITIZE_STRING);
+            $placesAssises = filter_var($_POST['FplacesAssises'], FILTER_SANITIZE_NUMBER_INT);
+            $placesDebout = filter_var($_POST['FplacesDebout'], FILTER_SANITIZE_NUMBER_INT);
 
-
-    public function execute(): string
-    {
-        // TODO
+            $db = InsertRepository::getInstance();
+            try {
+                $db->ajouterLieu($nomLieu, $adresse, $placesAssises, $placesDebout);
+                $res = '<h1>Lieu ajouté</hh1>';
+            } catch (PDOException $e) {
+                $res = '<h1>Erreur lors de lajout du lieu</h1>';
+            }
+        }
+        return $res;
     }
 }
