c9c3278ce62bc02c3aa437fcfd9033a9de335d12
Kuuro57 <noahallart57@gmail.com>
2024-11-07 18:00:41 +0100

    Ajout des classes Audio, Image, Style et Video. Modification des classes Artiste et Spectacle

---
+++ src/festival/Audio.php
@@ -0,0 +1,31 @@
+<?php declare (strict_types=1);
+
+namespace iutnc\sae_dev_web\festival;
+
+
+
+/**
+ * Classe qui représente une vidéo
+ */
+class Audio {
+
+    // Attribut
+    private int $idAudio; // Id de l'audio
+    private int $idSpectacle; // Id du spectacle auquel appartient l'audio
+    private string $nomFichierAudio; // Nom du fichier de l'audio
+
+
+
+    /**
+     * Constructeur de la classe
+     * @param int $idA Id de l'audio
+     * @param int $idS Id du spectacle
+     * @param string $nomFichAudio Nom du fichier de l'audio
+     */
+    public function __construct(int $idA, int $idS, string $nomFichAudio) {
+        $this->idAudio = $idA;
+        $this->idSpectacle = $idS;
+        $this->nomFichierAudio = $nomFichAudio;
+    }
+
+}
