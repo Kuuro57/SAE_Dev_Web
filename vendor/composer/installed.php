96204b2a1d9271a7bdd18b7181f87ff5e1e73c3c
armbrust5u <SuperBidul1@gmail.com>
2024-11-07 13:14:45 +0100

    Ajout de l'auto loader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 's3/sae_dev_web',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '02f2d27f74cf7491ec93b993c05f885d6eff25ce',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        's3/sae_dev_web' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '02f2d27f74cf7491ec93b993c05f885d6eff25ce',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
