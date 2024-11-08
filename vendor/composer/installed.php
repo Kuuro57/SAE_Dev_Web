2ac7d85ec83d3786417483e522ae9dec7425420b
Kuuro57 <noahallart57@gmail.com>
2024-11-08 14:40:42 +0100

    Modification que .gitignore et ajout du dossier vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iutnc/sae_dev_web',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '964f00a85757bec478585f14caea8fdd8feba2ba',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iutnc/sae_dev_web' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '964f00a85757bec478585f14caea8fdd8feba2ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
