d6a9c7b335225fe9ba53053a1c62cbd04ec52701
armbrust5u <SuperBidul1@gmail.com>
2024-11-08 09:40:31 +0100

    ajout getArtistes, getLieux, getStyles

--- src/repository/SelectRepository.php
+++ src/repository/SelectRepository.php
@@ -5,7 +5,8 @@ namespace iutnc\sae_dev_web\repository;
 /**
  * Classe qui récupère des informations auprès de la BDD
  */
-class SelectRepository extends Repository {
+class SelectRepository extends Repository
+{
 
     // Attribut
     private static ?SelectRepository $instance = null; // Instance unique de la classe SelectRepository
@@ -15,9 +16,10 @@ class SelectRepository extends Repository {
      * Méthode getInstance qui retourne une instance de SelectRepository
      * @return SelectRepository Une instance de la classe
      */
-    public static function getInstance(): SelectRepository {
+    public static function getInstance(): SelectRepository
+    {
 
-        if(self::$instance === null) {
+        if (self::$instance === null) {
             self::$instance = new SelectRepository(self::$config);
         }
         return self::$instance;
@@ -31,9 +33,10 @@ class SelectRepository extends Repository {
      * @return Spectacle[] La liste de tout les spectacles dans le bon ordre d'affichage
      */
 
-    public function getSpectacles(string $filtre) : array {  //default affiche ordre date | date ordre date |lieu ordre lieu| style ordre style
+    public function getSpectacles(string $filtre): array
+    {  //default affiche ordre date | date ordre date |lieu ordre lieu| style ordre style
         // Requête SQL en fonction du filtre
-        switch ($filtre){
+        switch ($filtre) {
             case "date":
                 $querySQL = "SELECT Spectacle.idSpectacle, Spectacle.idStyle, Spectacle.idArtiste, descSpectacle, Soiree.dateSoiree
                              FROM Spectacle INNER JOIN Programme ON Spectacle.idSpectacle = Programme.idSpectacle 
@@ -77,7 +80,8 @@ class SelectRepository extends Repository {
      * @param int $id Id du spectacle
      * @return Spectacle Un objet de type spectacle
      */
-    public function getSpectacle(int $id) : Spectacle {
+    public function getSpectacle(int $id): Spectacle
+    {
         // Requête SQL qui récupère l'id du spectacle
         $querySQL = "SELECT Spectacle.idSpectacle, Spectacle.idStyle, Spectacle.idArtiste, descSpectacle, 
                      FROM spectacle WHERE idSpectacle = ?";
@@ -103,7 +107,8 @@ class SelectRepository extends Repository {
      * @return array Liste qui contient le mot de passe (hashé) et le role de l'utilisateur si il est déjà
      *               présente dans la BDD, une liste vide si l'utilisateur n'existe pas
      */
-    public function findExistingEmail(string $email) : array {
+    public function findExistingEmail(string $email): array
+    {
         // Requête SQL
         $querySQL = "SELECT mdp, role FROM utilisateur WHERE email = ? ";
         // Préparation de la requête
@@ -118,8 +123,7 @@ class SelectRepository extends Repository {
         if (empty($data)) {
             // Le résultat est une liste vide
             $res = [];
-        }
-        //Sinon
+        } //Sinon
         else {
             // Le résultat est une liste contenant le mot de passe hashé correspondant à l'email et son role
             $res = [
@@ -131,11 +135,63 @@ class SelectRepository extends Repository {
         return $res;
     }
 
-    // TODO public function getDebutSpectacle
+    public function getArtistes() : array {
+        // Requête SQL qui récupère les attributs d'artistes
+        $querySQL = "Select Artiste.idArtiste, Artiste.nomArtiste FROM Artiste";
 
-    // TODO getArtistes()
+        // Préparation de la requête
+        $statement = $this->pdo->prepare($querySQL);
 
-    // TODO getStyles()
+        // Execution de la requête
+        $statement->execute();
 
-    // TODO getLieux() -> id + nom | ou tout
+        // On récupère les données sorties par la requête
+        $res = [];
+        foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $data) {
+            $artiste = new Artiste($this->$data['idArtiste'], $data['nomArtiste']);
+            $res[] = $artiste;
+        }
+        return $res;
+    }
+
+
+    public function getStyles() : array {
+        // Requête SQL qui récupère les attributs d'artistes
+        $querySQL = "Select Style.idStyle, Style.nomStyle FROM Style";
+
+        // Préparation de la requête
+        $statement = $this->pdo->prepare($querySQL);
+
+        // Execution de la requête
+        $statement->execute();
+
+        // On récupère les données sorties par la requête
+        $res = [];
+        foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $data) {
+            $style = new Style($this->$data['idStyle'], $data['nomStyle']);
+            $res[] = $style;
+        }
+        return $res;
+    }
+
+    
+    public function getLieux() : array {
+        // Requête SQL qui récupère les attributs d'artistes
+        $querySQL = "Select idLieu, nomLieu, adresse, nbPlacesAssises, nbPlacesDebout FROM lieux";
+
+        // Préparation de la requête
+        $statement = $this->pdo->prepare($querySQL);
+
+        // Execution de la requête
+        $statement->execute();
+
+        // On récupère les données sorties par la requête
+        $res = [];
+        foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $data) {
+            //modifier par rapport au constructeur de style
+            $lieux = new Style($this->$data['idLieux'], $data['nomLieux'], $data['adresse'], $data['nbPlacesAssises'], $data['nbPlacesDebout']);
+            $res[] = $lieux;
+        }
+        return $res;
+    }
 }
