d452c92fdc64b992f74b7061e5631e758b582e4e
OscarTom54 <oscartom54@gmail.com>
2024-11-08 09:27:34 +0100

    AddSoiree pasfini

--- src/action/AddSoireeAction.php
+++ src/action/AddSoireeAction.php
@@ -3,6 +3,7 @@
 namespace iutnc\sae_dev_web\action;
 
 use iutnc\sae_dev_web\repository\InsertRepository;
+use iutnc\sae_dev_web\repository\SelectRepository;
 use PDOException;
 
 
@@ -11,31 +12,47 @@ use PDOException;
  */
 class AddSoireeAction extends Action {
 
-    private string $formulaire =
-        '<form method="post" action="?action=add-soiree">
-        <input type="text" name="FnomSoiree" placeholder="Nom" class="input-field" required autofocus>' .
-        // ComboBox DropDown de touts les lieux
-        '<input type="checkbox" name="FestAnnule" placeholder="Est annulé ?" required>
-        <input type="date" name="FdateSoiree" placeholder="date soirée">
-        <input type="time" name="FheureDebut" placeholder="Heure début" class="input-field" required>
-        <input type="submit" name="connex" value="Ajouter" class="button">
+    private string $formulaire;
+
+    public function getForm(): string {
+        $lieuOptions = $this->getLieuOptions();
+        return '<form method="post" action="?action=add-soiree">
+            <input type="text" name="FnomSoiree" placeholder="Nom" class="input-field" required autofocus>
+            <select name="FidLieu" class="input-field" required>' . $lieuOptions . '</select>
+            <input type="checkbox" name="FestAnnule" placeholder="Est annulé ?" required>
+            <input type="date" name="FdateSoiree" placeholder="date soirée" required>
+            <input type="time" name="FheureDebut" placeholder="Heure début" class="input-field" required>
+            <input type="submit" name="connex" value="Ajouter" class="button">
         </form>';
+    }
+
+    private function getLieuOptions(): string {
+        $repo = SelectRepository::getInstance();
+        $lieux = $repo->getLieux();
+        $options = '';
+        foreach ($lieux as $lieu) {
+            $options .= '<option value="' . htmlspecialchars($lieu['id']) . '">' . htmlspecialchars($lieu['nom']) . '</option>';
+        }
+        return $options;
+    }
 
     public function execute(): string {
         if ($this->http_method == "GET") {
+            $formulaires = $this->getForm();
+
             $res = '<h1>Ajouter une soirée</h1>' . $this->formulaire;
         } else {
             $nomSoiree = filter_var($_POST['FnomSoiree'], FILTER_SANITIZE_STRING);
-            // TODO $idLieu = ??
-            $estAnnule = filter_var($_POST['FestAnnule'], FILTER_SANITIZE_NUMBER_INT); // FILTRER BOOL ?
+            $idLieu = filter_var($_POST['FidLieu'], FILTER_SANITIZE_NUMBER_INT);
             $dateSoiree = filter_var($_POST['FdateSoiree'], FILTER_SANITIZE_STRING); // FILTRER DATE ?
             $heureDebut = filter_var($_POST['FheureDebut'], FILTER_SANITIZE_STRING); // FILTRER HEURE ?
+            $estAnnule = filter_var($_POST['FestAnnule'], FILTER_SANITIZE_NUMBER_INT); // FILTRER BOOL ?
 
 
 
             $db = InsertRepository::getInstance();
             try {
-                $db->ajouterSoiree($nomSoiree, $idLieu, $estAnnule, $dateSoiree, $heureDebut);
+                $db->ajouterSoiree($nomSoiree, $idLieu, $dateSoiree, $estAnnule);
                 $res = '<h1>Soirée ajouté</hh1>';
             } catch (PDOException $e) {
                 $res = '<h1>Erreur lors de lajout de la soiree</h1>';
