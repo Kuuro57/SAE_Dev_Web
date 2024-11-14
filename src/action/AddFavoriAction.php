bfdbcf0f84a1407a0faca49e35e7f40f8e933131
ClemDW <clementdewasch54@gmail.com>
2024-11-14 10:20:39 +0100

    Ajout de la classe AddFavoriAction.php

---
+++ src/action/AddFavoriAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Super\SaeDevWeb\action;
+
+use iutnc\sae_dev_web\action\Action;
+
+class AddFavoriAction extends Action {
+
+    public function execute(): string {
+
+        $res = '';
+        $favs = array();
+
+
+
+        return $res;
+
+    }
+
+}
