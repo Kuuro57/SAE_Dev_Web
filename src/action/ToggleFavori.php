ef3f3a892aedc7df2304c833dc8ed04e74362874
ClemDW <clementdewasch54@gmail.com>
2024-11-14 11:18:50 +0100

    Ajout de la classe ToggleFavori.php

---
+++ src/action/ToggleFavori.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Super\SaeDevWeb\action;
+
+use iutnc\sae_dev_web\action\Action;
+
+class ToggleFavori extends Action {
+
+    public function execute(): string {
+
+        $res = "";
+
+
+        return $res;
+
+    }
+}
