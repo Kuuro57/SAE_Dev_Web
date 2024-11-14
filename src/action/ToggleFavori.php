ff21894600d35b680108f1e5ed5ece65e0b25a30
ClemDW <clementdewasch54@gmail.com>
2024-11-14 11:58:16 +0100

    Création de ToggleFavori.php pour gérer la liste de fav

--- src/action/ToggleFavori.php
+++ src/action/ToggleFavori.php
@@ -6,10 +6,17 @@ use iutnc\sae_dev_web\action\Action;
 
 class ToggleFavori extends Action {
 
+    private array $favs;
     public function execute(): string {
 
         $res = "";
 
+        $spec = $_GET["idSp"];
+        $favs[] = $spec;
+        $ids = serialize($favs);
+        $exp = time() + 30 * 24 * 60 * 60;
+
+        setcookie("Favoris", $ids, $exp);
 
         return $res;
 
