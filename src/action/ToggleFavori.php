4dc8762dbdfe8dc1135812f05f3f3b5c72b12d2a
ClemDW <clementdewasch54@gmail.com>
2024-11-15 10:31:33 +0100

    Gestion des favoris, manque l'affichage boutons et redirection

--- src/action/ToggleFavori.php
+++ src/action/ToggleFavori.php
@@ -1,24 +1,66 @@
 <?php
 
-namespace Super\SaeDevWeb\action;
+namespace iutnc\sae_dev_web\action;
 
 use iutnc\sae_dev_web\action\Action;
 
 class ToggleFavori extends Action {
 
-    private array $favs;
     public function execute(): string {
 
         $res = "";
 
-        $spec = $_GET["idSp"];
-        $favs[] = $spec;
-        $ids = serialize($favs);
-        $exp = time() + 30 * 24 * 60 * 60;
+        if($_GET["state"] == "add") {
 
-        setcookie("Favoris", $ids, $exp);
+            if (isset($_COOKIE["Favoris"])) {
+
+                $spec = $_GET["idSp"];
+                $val = ($_COOKIE["Favoris"]);
+                $favs[] = $val;
+                $favs[] = $spec;
+                $ids = (implode($favs));
+                $exp = time() + 30 * 24 * 60 * 60;
+
+                setcookie("Favoris", $ids, $exp);
+
+            } else {
+
+                $spec = $_GET["idSp"];
+                $favs[] = $spec;
+                $ids = (implode($favs));
+                setcookie("Favoris", $ids, time() + 30 * 24 * 60 * 60);
+
+            }
+        }
+
+        if($_GET["state"] == "sup") {
+            if (isset($_COOKIE["Favoris"])) {
+
+                $spec = $_GET["idSp"];
+                $val = ($_COOKIE["Favoris"]);
+                $favs = $this->toArray($val);
+                $index = array_search($spec, $favs);
+                if($index !== false) {
+                    unset($favs[$index]);
+                }
+                $favs = array_values($favs);
+
+                setcookie("Favoris", implode($favs), time() + 30 * 24 * 60 * 60);
+
+            }
+        }
 
         return $res;
 
     }
+
+    public function toArray(string $s): array {
+        $res = [];
+        for($i = 0; $i < strlen($s); $i++) {
+            array_push($res, $s[$i]);
+        }
+
+        return $res;
+    }
+
 }
